953221286f4ea41a0aa60413084c165fad229656
dmonakhov <mjelnr@gmail.com>
2016-12-15 18:52:35 +0100

    New distance calculation rules

--- QJFX/src/main/java/org/torquemada/q/controller/impl/QUtils.java
+++ QJFX/src/main/java/org/torquemada/q/controller/impl/QUtils.java
@@ -1,17 +1,19 @@
 package org.torquemada.q.controller.impl;
 
 import org.torquemada.q.model.contract.Direction;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by torquemada on 13.10.16.
+ * Calculation of next selected ball.
  */
 public class QUtils {
 
     private static float RIGHT_EST = 1.0f;
     private static float DIAG_EST = 0.3f;
+    private static Direction theLastDirection;
+    private static Stack<Integer> stack = new Stack<>();
 
     private static float calculateDistanceFor(int ball, int selectedId, int col, Direction action) {
         int candidateRow = ball / col;
@@ -22,35 +24,28 @@ public class QUtils {
         int lambdaRow = Math.abs(candidateRow - selectedRow);
         int lambdaCol = Math.abs(candidateCol - selectedCol);
 
-//        boolean rowLambdaLessThanColLambda = lambdaRow <= lambdaCol;
-//        boolean rowLambdaMoreThanColLambda = lambdaRow >= lambdaCol;
-
         switch(action) {
-            case Right : return candidateCol > selectedCol /*&& rowLambdaLessThanColLambda */?
-                    lambdaCol * RIGHT_EST + lambdaRow * DIAG_EST : Float.NaN;
-            case Left: return candidateCol < selectedCol /*&& rowLambdaLessThanColLambda */?
-                    lambdaCol * RIGHT_EST + lambdaRow * DIAG_EST : Float.NaN;
-            case Up : return candidateRow < selectedRow /*&& rowLambdaMoreThanColLambda */?
-                    lambdaRow * RIGHT_EST + lambdaCol * DIAG_EST : Float.NaN;
-            case Down: return candidateRow > selectedRow /*&& rowLambdaMoreThanColLambda */?
-                    lambdaRow * RIGHT_EST + lambdaCol * DIAG_EST : Float.NaN;
+            case Right : return candidateCol > selectedCol ? lambdaCol * RIGHT_EST + lambdaRow * DIAG_EST : Float.NaN;
+            case Left: return candidateCol < selectedCol ? lambdaCol * RIGHT_EST + lambdaRow * DIAG_EST : Float.NaN;
+            case Up : return candidateRow < selectedRow ? lambdaRow * RIGHT_EST + lambdaCol * DIAG_EST : Float.NaN;
+            case Down: return candidateRow > selectedRow ? lambdaRow * RIGHT_EST + lambdaCol * DIAG_EST : Float.NaN;
         }
         return Float.NaN;
     }
 
     public static int calculatePreferredToSelect(ArrayList<Integer> balls, int selectedId, int col, Direction action) {
-        float minDistance = Float.MAX_VALUE;
-        int selectedBall = selectedId;
+        if (theLastDirection != action) {
+            stack.clear();
+            theLastDirection = action;
+            HashMap<Integer, Float> m = new HashMap<>();
+            balls.forEach((b) -> m.put(b, calculateDistanceFor(b, selectedId, col, action)));
 
-        for (int ball : balls) {
-            if (ball == selectedId) continue;
-            float calculateDistance = calculateDistanceFor(ball, selectedId, col, action);
-            if (calculateDistance < minDistance) {
-                minDistance = calculateDistance;
-                selectedBall = ball;
-            }
+            stack.addAll(balls.stream().filter((b) -> !m.get(b).isNaN()).sorted(
+                    (b1, b2) -> m.get(b1) > m.get(b2) ? -1 : m.get(b1) < m.get(b2) ? 1 : 0).collect(Collectors.toList()));
         }
-        return selectedBall;
+
+        if (stack.isEmpty()) return selectedId;
+        else return stack.pop();
     }
 
 
